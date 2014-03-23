660627d83b6f749cde23fc4fd7a6442247e83117
reachh <irina.bs@abv.bg>
2014-03-23 19:07:36 +0200

    Adding solution and test

---
+++ prepare_meal/solution.py
@@ -0,0 +1,13 @@
+def prepare_meal(number):
+    result = ''
+    while number % 3 == 0:
+        result = result + ' spam'
+        number = number // 3
+    while number % 5 == 0:
+        if result == '':
+            result = ' eggs'
+            number = number // 5
+        else:
+            result = result + ' and eggs'
+            number = number // 5
+    return result
---
+++ prepare_meal/test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from solution import prepare_meal
+
+class TestPrepareMeal(unittest.TestCase):
+    def test_prepare_meal(self):
+        self.assertEqual(' spam', prepare_meal(3))
+        self.assertEqual(' spam spam spam',prepare_meal(27))
+        self.assertEqual('', prepare_meal(7))
+        self.assertEqual(' eggs', prepare_meal(5))
+        self.assertEqual(' spam and eggs', prepare_meal(15))
+        self.assertEqual(' spam spam and eggs', prepare_meal(45))
+        self.assertEqual(" eggs", prepare_meal(5))
+        self.assertEqual(' spam', prepare_meal(3))
+        self.assertEqual(' spam spam spam', prepare_meal(27))
+        self.assertEqual(' spam and eggs', prepare_meal(15))
+        self.assertEqual(' spam spam and eggs', prepare_meal(45))
+        self.assertEqual('', prepare_meal(7))
+
+
+if __name__ == '__main__':
+    unittest.main()
